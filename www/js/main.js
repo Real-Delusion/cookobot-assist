ab1fcfd5be47d206b54b4bbc6ea089c8f8af39f9
Joan Ciprià <joancipria@gmail.com>
2020-04-21 19:41:09 +0200

    Install vue and basic notifications

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,29 @@
+/* -----------------------
+--------- VUE JS ---------
+--------------------------*/
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        connected: false
+    }
+})
+
+
+/* -----------------------
+--------- Socket ---------
+--------------------------*/
 var socket = io('http://192.168.1.124:3000');
-socket.on('connect', function () { });
+socket.on('connect', () => {
+    app.connected = true
+});
 socket.on('event', function (data) { });
-socket.on('disconnect', function () { });
+socket.on('disconnect', () => {
+    app.connected = false
+});
 
 function sendIssue() {
     let message = document.getElementById('message').value.toString();
     socket.emit('issue', message);
 }
+
